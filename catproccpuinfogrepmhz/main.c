348a7762c98d6bfb9c83d5a4faa05bd619582bd0
ivfiev <filjko@ya.ru>
2024-07-20 19:20:44 +0100

    first pass

--- catproccpuinfogrepmhz/main.c
+++ catproccpuinfogrepmhz/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <ctype.h>
 
 int get_cpus() {
   char buf[1024];
@@ -9,14 +12,14 @@ int get_cpus() {
 
   fp = popen("lscpu", "r"); 
   if (fp == NULL) {
-    printf("failed to run lscpu");
+    printf("failed to run lscpu\n");
     exit(1);
   }
 
   for (;;) {
     char *s = fgets(buf, sizeof(buf), fp);
     if (s == NULL) {
-      printf("failed to read lscpu output");
+      printf("failed to read lscpu output\n");
       exit(1);
     }
 
@@ -26,12 +29,12 @@ int get_cpus() {
     }
   }
 
-  while (*line && (*line < '0' || '9' < *line)) {
+  while (*line && !isdigit(*line)) {
     line++;
   }
 
   if (!*line) {
-    printf("failed to parse the number of CPUs");
+    printf("failed to parse the number of CPUs\n");
     exit(1);
   }
 
@@ -40,8 +43,89 @@ int get_cpus() {
   return atoi(line);
 }
 
+char *read_cpuinfo() {
+  const int size = 64 * 1024;
+  char *buf = calloc(size, sizeof(char));
+  int fp, bytes, len;
+
+  len = 0;
+  fp = open("/proc/cpuinfo", O_RDONLY);
+  while ((bytes = read(fp, buf + len, size - 1 - len)) > 0) {
+    len += bytes;
+  }
+
+  if (bytes < 0) {
+    printf("failed to read cpuinfo\n");
+    exit(1);
+  }
+
+  close(fp);
+  return buf;
+}
+
+void index_cpuinfo(int indexes[], int cpus) {
+  char *cpuinfo = read_cpuinfo();
+  char *ix = cpuinfo;
+  int cpu = 0;
+
+  while (cpu < cpus) {
+    ix = strstr(ix, "cpu MHz");
+    if (ix) {
+      indexes[cpu++] = ix - cpuinfo;
+      ix++;
+    }
+  }
+
+  if (cpu != cpus) {
+    printf("failed to index cpuinfo\n");
+    exit(1);
+  }
+  
+  free(cpuinfo);
+}
+
+void set_clocks(int clocks[], int indexes[], int cpus) {
+  char *cpuinfo = read_cpuinfo();
+
+  for (int i = 0; i < cpus; i++) {
+    char *ix = cpuinfo + indexes[i] - cpus;
+    ix = strstr(ix, "MHz");
+
+    while (!isdigit(*ix)) {
+      ix++;
+    }
+    clocks[i] = atoi(ix);
+  }
+
+  free(cpuinfo);
+}
 
 int main(void) {
-  int cpus = get_cpus();
+  int cpus = get_cpus(), samples = 0;
+  int indexes[cpus], clocks[cpus], avgs[cpus], maxes[cpus];
+
+  memset(avgs, 0, cpus * sizeof(int));
+  memset(maxes, 0, cpus * sizeof(int));
+  index_cpuinfo(indexes, cpus);
+
+  for (;;) {
+    for (int i = 0; i < 10; i++) {
+      set_clocks(clocks, indexes, cpus);
+      
+      for (int cpu = 0; cpu < cpus; cpu++) {
+        maxes[cpu] = maxes[cpu] < clocks[cpu] ? clocks[cpu] : maxes[cpu];
+        avgs[cpu] = (double)(avgs[cpu] * samples + clocks[cpu]) / (samples + 1);
+      }
+      samples++;
+      usleep(100 * 1000);
+    }
+    
+    printf("\e[1;1H\e[2J");
+    printf("CORE#\tNow\tMax\tAvg\n");
+    for (int i = 0; i < cpus; i++) {
+      printf("%d\t%d\t%d\t%d\n", i, clocks[i], maxes[i], avgs[i]);
+    }
+  }
+
   return 0;
 }
