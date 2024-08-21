96807415caf76b098e5e9b7a0718593fcdef5616
ivfiev <filjko@ya.ru>
2024-08-21 22:27:32 +0100

    improve script

--- p2p/run.sh
+++ p2p/run.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
-# Function to start X instances of p2p
 start_p2p() {
+    arg="peers"
     for i in {0..9}
     do
         port=$((8080 + i))
         ./p2p "$port" &  # --logs 1065 &
         echo "Started p2p instance on port $port"
+        arg="$arg,$port"
     done
+    if [ "$1" == "-n" ]; then
+        echo "$arg"
+        echo "$arg" | nc localhost 8080
+    fi
 }
 
-# Function to kill all running instances of p2p
 kill_p2p() {
     pkill p2p
     echo "Killed all p2p instances"
 }
 
-# Change directory to 'cmake-build' if it exists
 if [ -d "cmake-build-debug" ]; then
     cd cmake-build-debug || exit
     echo "Changed directory to cmake-build-debug"
 fi
 
-# Check the command-line arguments
 if [ "$1" == "-u" ]; then
-    start_p2p
+    start_p2p $2
 elif [ "$1" == "-d" ]; then
     kill_p2p
 else
