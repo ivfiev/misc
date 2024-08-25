e3eaa691241f55ce77262579da9461a34fd6d76e
ivfiev <filjko@ya.ru>
2024-08-25 13:26:47 +0100

    ordering events

--- p2p/main.c
+++ p2p/main.c
@@ -7,6 +7,17 @@ void init(char *port);
 
 void init_log(char *port);
 
+int is_timer(int fd);
+
+int cmp_events(struct epoll_event *e1, struct epoll_event *e2) {
+  epoll_cb *cb1 = e1->data.ptr;
+  epoll_cb *cb2 = e2->data.ptr;
+  printf("qsort %d %d\n", cb1->fd, cb2->fd);
+  int t1 = is_timer(cb1->fd);
+  int t2 = is_timer(cb2->fd);
+  return t1 - t2;
+}
+
 int main(int argc, char **argv) {
   srand(time(NULL));
   struct epoll_event events[EPOLL_MAX_EVENTS];
@@ -22,6 +33,8 @@ int main(int argc, char **argv) {
 
   for (;;) {
     int ready = epoll_wait(EPFD, events, EPOLL_MAX_EVENTS, -1);
+
+    qsort(events, ready, sizeof(struct epoll_event), (__compar_fn_t)cmp_events);
 
     for (int i = 0; i < ready; i++) {
       epoll_cb *cb = events[i].data.ptr;
