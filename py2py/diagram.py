f0c500a008da243426c14972a559288c8b317711
ivfiev <filjko@ya.ru>
2024-08-26 20:25:19 +0100

    use digraphs

--- py2py/diagram.py
+++ py2py/diagram.py
@@ -12,7 +12,7 @@ class Diagram(Output):
 
     def redraw(self):
         plt.clf()
-        G = nx.Graph()
+        G = nx.DiGraph()
         for node in self.model.graph:
             G.add_node(node, label=node)
         for node, edges in self.model.graph.items():
@@ -20,7 +20,7 @@ class Diagram(Output):
                 G.add_edge(node, edge)
         pos = nx.circular_layout(G)
         labels = nx.get_node_attributes(G, 'label')
-        nx.draw(G, pos, with_labels=False, node_size=1000, node_color='lightblue', edge_color='gray', font_size=10)
+        nx.draw(G, pos, with_labels=False, node_size=1000, node_color='lightblue', edge_color='gray', font_size=10, arrows=True, arrowstyle='-|>')
         nx.draw_networkx_labels(G, pos, labels, font_size=10, font_family='sans-serif')
         plt.draw()
         plt.pause(0.025)
