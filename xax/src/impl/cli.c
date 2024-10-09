c80155d3d7cd2b72b07f2b08415a872bf34d1f74
ivfiev <filjko@ya.ru>
2024-10-09 23:14:09 +0100

    cli range

--- xax/src/impl/cli.c
+++ xax/src/impl/cli.c
@@ -18,6 +18,7 @@ static int use_stdin(void) {
   return !strcmp("--stdin", args_get("arg2"));
 }
 
+// TODO simplify, get rid of 'equal' & don't parse integers
 static union word64 parse_value(char *val_str) {
   char *end = NULL;
   union word64 ret;
@@ -32,11 +33,21 @@ static union word64 parse_value(char *val_str) {
   return ret;
 }
 
+static float parse_float(char *val_str) {
+  float ret;
+  char *end = NULL;
+  ret = (float)strtod(val_str, &end);
+  if (*end != 0) {
+    err_fatal("parse_float");
+  }
+  return ret;
+}
+
 static uintptr_t parse_addr(char *addr_str) {
   char *end = NULL;
   uintptr_t addr = strtoull(addr_str, &end, 16);
   if (*end != 0) {
-    err_fatal("parse_value");
+    err_fatal("parse_addr");
   }
   return addr;
 }
@@ -94,8 +105,32 @@ void set32(void) {
   write_mem(fd, addr, value.bytes, 4);
 }
 
+void range32(void) {
+  char *proc_name = args_get("arg0");
+  char *range_str = args_get("arg1");
+  OPEN_MEM(proc_name);
+  char from_str[16], to_str[16];
+  sscanf(range_str, "(%15[^,],%15[^)])", from_str, to_str);
+  float from = parse_float(from_str);
+  float to = parse_float(to_str);
+  mem_desc ds[1024];
+  size_t ds_size = read_mem_desc(pid, ds, SIZEARR(ds));
+  for (int i = 0; i < ds_size; i++) {
+    mem_desc desc = ds[i];
+    mem_block *block = read_mem_block(fd, desc.start, desc.size);
+    SCAN(block, {
+      if (IN_RANGE(from, word.int32, to)) {
+        printf("%d 0x%lx 0x%lx %d\n", i, WORD_ADDR, offset, word.int32);
+      } else if (IN_RANGE(from, word.float32, to)) {
+        printf("%d 0x%lx 0x%lx %f\n", i, WORD_ADDR, offset, word.float32);
+      }
+    });
+  }
+}
+
 __attribute__((constructor))
 static void init(void) {
   args_add("equal32", equal32);
   args_add("set32", set32);
+  args_add("range32", range32);
 }
