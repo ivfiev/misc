2fd6878fd518f3264ebadaeaca91ef566d66869f
ivfiev <filjko@ya.ru>
2024-10-10 20:19:27 +0100

    wip

---
+++ xax/src/impl/cli32.c
@@ -0,0 +1,179 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "args.h"
+#include "scan.h"
+#include "util.h"
+#include "proc.h"
+
+#define OPEN_MEM(proc_name) \
+  pid_t pid = get_pid(proc_name); \
+  if (pid <= 0) { \
+    err_fatal("pid"); \
+  } \
+  int fd = open_mem(pid)   \
+
+#define PARSE_RANGE() \
+  char from_str[16], to_str[16]; \
+  if (sscanf(range_str, "(%15[^,],%15[^)])", from_str, to_str) != 2) { \
+    err_fatal("bad range32 input"); \
+  } \
+  float from = parse_value(from_str, FLOAT32_TYPE).float32; \
+  float to = parse_value(to_str, FLOAT32_TYPE).float32                \
+
+// TODO delet?
+#define FOREACH_BLOCK(code) \
+  do {                          \
+  mem_desc ds[1024]; \
+  size_t ds_size = read_mem_desc(pid, ds, SIZEARR(ds));  \
+  for (int i = 0; i < ds_size - 3; i++) { \
+    mem_desc desc = ds[i]; \
+    mem_block *block = read_mem_block(fd, desc.start, desc.size); \
+    code \
+    free_mem(block); \
+  }                         \
+  } while (0)               \
+
+#define READ_DS(size) \
+  mem_desc ds[size]; \
+  size_t ds_size = read_mem_desc(pid, ds, SIZEARR(ds))
+
+#define INFER_TYPE 0
+#define FLOAT32_TYPE 1
+
+
+static int use_stdin(void) {
+  return !strcmp("--stdin", args_get("arg2"));
+}
+
+static union word64 parse_value(char *val_str, int type) {
+  char *end = NULL;
+  union word64 ret;
+  if (type == INFER_TYPE) {
+    if (strstr(val_str, ".") != NULL) {
+      ret.float32 = (float)strtod(val_str, &end);
+    } else {
+      ret.int32 = (int)strtol(val_str, &end, 10);
+    }
+  } else {
+    if (type == FLOAT32_TYPE) {
+      ret.float32 = (float)strtod(val_str, &end);
+    }
+  }
+  if (end == NULL || *end != 0) {
+    err_fatal("parse_value");
+  }
+  return ret;
+}
+
+static uintptr_t parse_addr(char *addr_str) {
+  char *end = NULL;
+  uintptr_t addr = strtoull(addr_str, &end, 16);
+  if (*end != 0) {
+    err_fatal("parse_addr");
+  }
+  return addr;
+}
+
+void set32(void) {
+  char *proc_name = args_get("arg0");
+  char *addr_str = args_get("arg1");
+  char *value_str = args_get("arg2");
+  OPEN_MEM(proc_name);
+  uintptr_t addr = parse_addr(addr_str);
+  union word64 value = parse_value(value_str, INFER_TYPE);
+  write_mem(fd, addr, value.bytes, 4);
+  close_mem(fd);
+}
+
+void range32(void) {
+  char *proc_name = args_get("arg0");
+  char *range_str = args_get("arg1");
+  OPEN_MEM(proc_name);
+  PARSE_RANGE();
+  FOREACH_BLOCK({
+    SCAN(block, {
+      if (IN_RANGE(from, (float)word.int32, to)) {
+        printf("%d 0x%lx 0x%lx %d\n", i, WORD_ADDR, offset, word.int32);
+      } else if (IN_RANGE(from, word.float32, to)) {
+        printf("%d 0x%lx 0x%lx %f\n", i, WORD_ADDR, offset, word.float32);
+      }
+    });
+  });
+  close_mem(fd);
+}
+
+void delta32(void) {
+  char *proc_name = args_get("arg0");
+  char *range_str = args_get("arg1");
+  OPEN_MEM(proc_name);
+  PARSE_RANGE();
+  char line[128];
+  while ((fgets(line, SIZEARR(line), stdin) != NULL)) {
+    union word64 word;
+    int ix;
+    uintptr_t word_addr, offset;
+    float prev;
+    if (sscanf(line, "%d %lx %lx %f", &ix, &word_addr, &offset, &prev) == 4) {
+      read_mem_bytes(fd, word_addr, word.bytes, 4);
+      if (word.float32 == prev) {
+        continue;
+      }
+      if (IN_RANGE(prev + from, (float)word.int32, prev + to)) {
+        printf("%d 0x%lx 0x%lx %d\n", ix, word_addr, offset, word.int32);
+      } else if (IN_RANGE(prev + from, word.float32, prev + to)) {
+        printf("%d 0x%lx 0x%lx %f\n", ix, word_addr, offset, word.float32);
+      }
+    } else {
+      printf("bad input: %s\n", line);
+    }
+  }
+}
+
+void info32(void) {
+  char *proc_name = args_get("arg0");
+  char *addr_str = args_get("arg1");
+  char *range_str = args_get("arg2");
+  OPEN_MEM(proc_name);
+  PARSE_RANGE();
+  uintptr_t addr = parse_addr(addr_str);
+  if (addr % 4 != 0) {
+    err_fatal("bad addrs");
+  }
+  READ_DS(1536);
+  for (uintptr_t ptr = addr + (int)from * 4; ptr <= addr + (int)to * 4; ptr += 4) {
+    int off = 0;
+    char line[128];
+    union word64 word = {.int64 = 0};
+    read_mem_bytes(fd, ptr, word.bytes, 4);
+    off += snprintf(line, SIZEARR(line), "0x%lx  ", ptr);
+    if (IN_RANGE(-10000000, word.int32, 10000000)) {
+      off += snprintf(line + off, SIZEARR(line) - off, "Integer %d", word.int32);
+    } else if (IN_RANGE(-1000000.0, word.float32, 1000000.0)) {
+      off += snprintf(line + off, SIZEARR(line) - off, "Float32 %f", word.float32);
+    } else {
+      int i;
+      for (i = 0; i < ds_size; i++) {
+        if (IN_RANGE(ds[i].start, word.ptr64, ds[i].start + ds[i].size)) {
+          off += snprintf(line + off, SIZEARR(line) - off, "Pointer 0x%lx", word.ptr64);
+          break;
+        }
+      }
+      if (i == ds_size) {
+        off += snprintf(line + off, SIZEARR(line) - off, "Unknown %lx", word.ptr64);
+      }
+    }
+    if (ptr == addr) {
+      snprintf(line + off, SIZEARR(line) - off, "  -----");
+    }
+    printf("%s\n", line);
+  }
+}
+
+__attribute__((constructor))
+static void init(void) {
+  args_add("set32", set32);
+  args_add("range32", range32);
+  args_add("delta32", delta32);
+  args_add("info32", info32);
+}
--- xax/src/impl/h3sod.c
+++ xax/src/impl/h3sod.c
@@ -1,6 +1,3 @@
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "UnusedValue"
-
 #include <stdio.h>
 #include <unistd.h>
 #include "args.h"
@@ -10,42 +7,11 @@
 #include "hashtable.h"
 
 static void run(void) {
-  mem_desc ds[4096];
-  pid_t pid = get_pid("h3sod");
-  size_t ds_count = read_mem_desc(pid, ds, SIZEARR(ds)) - 1000;
-  int fd = open_mem(pid);
-//  mem_desc *desc = find_mem_desc("/home/fi/Downloads/Heroes", ds, ds_count);
-//  uint64_t offset = 0x20ba4;
-//  uintptr_t addr = 0x5500ba4;
-//  write_mem(fd, addr, (union word64){.int32=9999999}.bytes, 4);
-  for (int i = 0; i < ds_count - 1; i++) {
-    mem_desc *desc = ds + i;
-    mem_block *block = read_mem_block(fd, desc->start, desc->size);
-    SCAN(block, {
-      if (word.int32 == 14675) {
-        printf("%s -> %lx, %lx\n", desc->name, WORD_ADDR, offset);
-      }
-    });
-    free_mem(block);
-  }
-  printf("sleeping... go sell some wood!\n");
-  sleep(10);
-  for (int i = 0; i < ds_count - 1; i++) {
-    mem_desc *desc = ds + i;
-    mem_block *block = read_mem_block(fd, desc->start, desc->size);
-    SCAN(block, {
-      if (word.int32 == 14700) {
-        printf("%s -> %lx, %lx\n", desc->name, WORD_ADDR, offset);
-      }
-    });
-    free_mem(block);
-  }
+  // 285 0x11e8c70 0x308c70 15         x coord
+  //
 }
 
 __attribute__((constructor))
 static void init(void) {
   args_add("h3sod", run);
 }
-
-
-#pragma clang diagnostic pop
