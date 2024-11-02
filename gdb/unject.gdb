c462a5ad64f4434c64c72e7ddb797a4d4a387608
ivfiev <filjko@ya.ru>
2024-11-02 18:46:38 +0000

    wip

--- gdb/unject.gdb
+++ gdb/unject.gdb
@@ -1,4 +1,4 @@
 set pagination off
-set $handle = 0x5bc21630
+set $handle = 0x5bc27d50
 call (void*) dlclose($handle)
 exit
--- ld_preload/32/hack.cxx
+++ ld_preload/32/hack.cxx
@@ -8,6 +8,11 @@ uintptr_t BASE_ADDR;
 uintptr_t FUNC_ADDR;
 uintptr_t DETOUR_ADDR;
 
+struct machine_code {
+  uint8_t bytes[16];
+  size_t size;
+};
+
 uintptr_t get_base_addr() {
   std::ifstream maps("/proc/self/maps");
   std::string line;
@@ -31,24 +36,26 @@ void print_bytes(uint8_t *ptr, size_t n) {
   }
 }
 
-void patch(uintptr_t ptr, uint8_t *bytes, size_t n) {
+machine_code patch(uintptr_t ptr, machine_code code) {
+  machine_code old;
+  old.size = code.size;
+  memcpy(old.bytes, (void *)ptr, old.size);
   if (mprotect((void *)(ptr - ptr % 0x1000), 0x1000, PROT_READ | PROT_WRITE | PROT_EXEC) != 0) {
     perror(strerror(errno));
   }
-  memcpy((void *)ptr, bytes, n);
+  memcpy((void *)ptr, code.bytes, code.size);
   if (mprotect((void *)(ptr - ptr % 0x1000), 0x1000, PROT_READ | PROT_EXEC) != 0) {
     perror(strerror(errno));
   }
+  return old;
 }
 
 void detour() {
   asm volatile (
     "push %%ebx \n\t"
-    "sub $0x14,%%esp \n\t"
+    "sub $0x4,%%esp \n\t"
   : : :);
   puts("detour!");
-  printf("0x%x\n", get_base_addr());
-  printf("0x%x\n", BASE_ADDR);
   asm volatile (
     "jmp *%0 \n\t" 
     : 
@@ -59,29 +66,55 @@ void detour() {
 
 void set_addrs() {
   BASE_ADDR = get_base_addr();
-  FUNC_ADDR = BASE_ADDR + 0x119d;
+  FUNC_ADDR = BASE_ADDR + 0x120c;
   DETOUR_ADDR = (uintptr_t)detour;
   printf("Base proc addr: [%p]\n", (void *)BASE_ADDR);
   printf("Func addr: [%p]\n", (void *)FUNC_ADDR);
   printf("Detour addr: [%p]\n", (void *)DETOUR_ADDR);
 }
 
+machine_code get_jmp_code() {
+  machine_code code;
+  code.size = 5;
+  code.bytes[0] = 0xE9;
+  *(uintptr_t *)(code.bytes + 1) = DETOUR_ADDR - FUNC_ADDR - code.size;
+  return code;
+}
+
+class Hoock {
+private:
+  machine_code old;
+public:
+  Hoock() {
+    set_addrs();
+  }
+
+  void write() {
+    machine_code code = get_jmp_code();
+    old = patch(FUNC_ADDR, code);
+  }
+
+  ~Hoock() {
+    puts("restoring original code...");
+    patch(FUNC_ADDR, old); 
+    puts("code restored");
+  }
+};
+
+Hoock *H = nullptr;
+
 __attribute__((constructor))
 void ctor() {
   puts("constructor");
-  // set_addrs();
-  // uint8_t instr[5];
-  // instr[0] = 0xE9;
-  // *(uintptr_t *)(instr + 1) = DETOUR_ADDR - FUNC_ADDR - sizeof(instr);
-  // patch(FUNC_ADDR, instr, sizeof(instr));
-  // print_bytes((uint8_t *)DETOUR_ADDR, 20);
+  H = new Hoock;
+  H->write();
   puts("exit constructor");
 }
-
 
 __attribute__((destructor))
 void dtor() {
   puts("destructor");
+  delete H;
   puts("exit destructor");
 }
 // static/dynamic ptr
