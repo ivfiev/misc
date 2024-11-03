0219a6e7633e35da1647092f68e43809119df5c2
ivfiev <filjko@ya.ru>
2024-11-03 23:09:07 +0000

    wip

--- h3so/main.cxx
+++ h3so/main.cxx
@@ -3,17 +3,14 @@
 #include <sys/mman.h>
 #include <string.h>
 #include <unistd.h>
-#include <exception>
 #include <thread>
-#include <signal.h>
-#include <csignal>
 
-typedef void (*award_xp_t)(uint32_t, uint32_t, uint8_t);
+typedef void (*award_xp_t)(uint32_t, uint32_t, uint32_t);
 
 award_xp_t AWARD_XP_FUNC = (award_xp_t)0x4e3620;
 
 __attribute__((optimize("O0")))
-award_xp_t award_xp(uintptr_t hero, uint32_t xp, uint32_t arg0, uint8_t arg1) {
+void award_xp(uintptr_t hero, uint32_t xp, uint32_t arg0, uint32_t arg1) {
   asm("mov %0, %%ecx" : : "r"(hero) : "%ecx");
   AWARD_XP_FUNC(xp, arg0, arg1);
 }
@@ -23,18 +20,17 @@ void donosink(int) {
 }
 
 void award_thread() {
-  signal(SIGILL, donosink);
   for (;;) {
-    sleep(1);
-    award_xp(0x5401640, 2000, 0, 1);
+    sleep(5);
+    award_xp(0x5501640, 2000, 0, 0);
   }
 }
 
 __attribute__((constructor))
 void ctor() {
   puts("constructor");
-  std::thread t {award_thread};
-  t.detach();
+  award_xp(0x5501640, 12000, 0, 0);
+  std::thread {award_thread}.detach();
   puts("exit constructor");
 }
 
@@ -43,3 +39,4 @@ void dtor() {
   puts("destructor");
   puts("exit destructor");
 }
+// logging to file
