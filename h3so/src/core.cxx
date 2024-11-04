1a3c0188ca1ab5232ea6821f7f64319be1bb091e
ivfiev <filjko@ya.ru>
2024-11-04 22:41:49 +0000

    xp forall

--- h3so/src/core.cxx
+++ h3so/src/core.cxx
@@ -2,27 +2,51 @@
 #include "stdint.h"
 #include <unistd.h>
 #include <thread>
+#include <stdexcept>
+
+#define STATIC_PTR 0x699538
 
 typedef void (*award_xp_t)(uint32_t, uint32_t, uint32_t);
-
 static award_xp_t AWARD_XP_FUNC = (award_xp_t)0x4e3620;
 
-__attribute__((optimize("O0")))
-void award_xp(uintptr_t hero, uint32_t xp, uint32_t arg0, uint32_t arg1) {
-  asm("mov %0, %%ecx" : : "r"(hero) : "%ecx");
-  AWARD_XP_FUNC(xp, arg0, arg1);
-}
+class H {
+private:
+  uintptr_t addr;
+public:
+  H(uintptr_t a) {
+    addr = a;
+  }
+  ~H() {}
+
+  __attribute__((optimize("O0")))
+  void award_xp(uint32_t xp) {
+    asm("mov %0, %%ecx" : : "r"(addr) : "%ecx");
+    AWARD_XP_FUNC(xp, 0, 0);
+  }
+};
+
+static H *Hs[156];
 
 void award_thread() {
   for (;;) {
     sleep(5);
-    award_xp(0x5501640, 2000, 0, 0);
+    for (auto h : Hs) {
+      h->award_xp(1000);
+    }
+  }
+}
+
+void init() {
+  uintptr_t state = *(uintptr_t *)STATIC_PTR + 0x21620;
+  log("state: [0x%lx]", state);
+  for (int i = 0; i < 156; i++) {
+    Hs[i] = new H(state + i * 0x492);
   }
 }
 
 void inject() {
+  init();
   std::thread {award_thread}.detach();
-  log("injected");
 }
 
 void unject() {
