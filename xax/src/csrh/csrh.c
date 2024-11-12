f725a8e50f098b771c7a968c75c2ea9e608a0621
ivfiev <filjko@ya.ru>
2024-11-12 23:27:59 +0000

    rh wip

--- xax/src/csrh/csrh.c
+++ xax/src/csrh/csrh.c
@@ -5,35 +5,30 @@
 #include "util.h"
 #include "impl.h"
 #include <string.h>
-#include "math.h"
-
-static uintptr_t OFFSETS_XYZ[] = {0x3948428, 0x688, 0x20, 0x344};
-static uintptr_t OFFSETS_PITCH[] = {0x3782160, 0x184};
-
-static uintptr_t hop(int mem_fd, uintptr_t base, uintptr_t offsets[], size_t size) {
-  union word64 word = {.ptr64 = base};
-  for (int i = 0; i < size - 1; i++) {
-    word = read_mem_word64(mem_fd, word.ptr64 + offsets[i]);
-  }
-  return word.ptr64 + offsets[size - 1];
-}
 
 static void run(void) {
   OPEN_MEM("cs2$");
   READ_DS(1536);
+  disable_stderr();
   uintptr_t base = ds[find_mem_desc("libclient", ds, ds_size)].start;
+  printf("0x%lx\n", base);
   for (;;) {
-    uintptr_t ptr_x = hop(fd, base, OFFSETS_XYZ, SIZEARR(OFFSETS_XYZ));
-    float x = read_mem_word32(fd, ptr_x).float32;
-    float y = read_mem_word32(fd, ptr_x + 4).float32;
-    float z = read_mem_word32(fd, ptr_x + 8).float32;
-    uintptr_t ptr_p = hop(fd, base, OFFSETS_PITCH, SIZEARR(OFFSETS_PITCH));
-    float pitch_r = asin(read_mem_word32(fd, ptr_p).float32);
-    float yaw_r = -atan2(read_mem_word32(fd, ptr_p - 16).float32, -read_mem_word32(fd, ptr_p - 32).float32);
-    float pitch_d = pitch_r / M_PI_2 * 90;
-    float yaw_d = yaw_r / M_PI * 180;
-    printf("(%f, %f, %f) (%f, %f)\n", x, y, z, pitch_d, yaw_d);
-    msleep(25);
+    uintptr_t ENTITY_LIST = read_mem_word64(fd, base + 0x3a2f5b0 - 0x100000).ptr64; // param_1 table ptr, param_2 [1..64]
+    puts("-----------");
+    printf("ENTITY LIST: 0x%lx\n", ENTITY_LIST);
+    for (int player = 1; player <= 64; player++) {
+      uintptr_t PLAYER_LIST = read_mem_word64(fd, ENTITY_LIST + 0x10).ptr64; // + (long)((int)player >> 9) * 8
+      uintptr_t puVar2 = PLAYER_LIST + player * 0x78;
+      uintptr_t deref = read_mem_word64(fd, puVar2).ptr64;
+      if (deref != 0) {
+        printf("PLAYER (game) ix & ptr %d: 0x%lx\n", player - 1, deref);
+        printf("PLAYER team: %d\n", read_mem_word32(fd, deref + 0x55b).int32);
+        printf("local player: 0x%lx\n", read_mem_word64(fd, base + 0x3a2b118 - 0x100000).ptr64);
+        printf("is alive?: %d\n", read_mem_word32(fd, deref + 0x99c).int32);
+      }
+    }
+
+    msleep(525);
   }
 }
 
@@ -41,6 +36,4 @@ __attribute__((constructor))
 static void init(void) {
   args_add("csrh", run);
 }
-
-// id t/ct
 // use gtk/imgui or make tk bearable
