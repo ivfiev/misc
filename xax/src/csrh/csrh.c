aea18c42d88e650153a8c27a84cddca9c877b07f
ivfiev <filjko@ya.ru>
2024-11-14 20:15:52 +0000

    yaw

--- xax/src/csrh/csrh.c
+++ xax/src/csrh/csrh.c
@@ -6,18 +6,22 @@
 #include "impl.h"
 #include "scan.h"
 #include <string.h>
+#include "math.h"
 
 #define START_ADDR 0x100000
 #define GLOBAL_STATE_PTR_ADDR (LIBCLIENT_BASE + 0x3a43f50 - START_ADDR)
 #define LOCAL_PLAYER_CTL_PTR_ADDR (LIBCLIENT_BASE + 0x3a3fab8 - START_ADDR)
+#define PAWN_ARRAY_ADDR (LIBCLIENT_BASE + 0x38c2260 - START_ADDR)
 
-static int MEM_FD;
-static uintptr_t LIBCLIENT_BASE;
+int MEM_FD;
+uintptr_t LIBCLIENT_BASE;
 
 struct player_descriptor {
+  uintptr_t ctl;
+  uintptr_t pawn;
   int is_local;
-  float x, y, z;
-  float pitch, yaw;
+  float x, y;
+  float yaw;
   int is_alive;
   char team;
 };
@@ -38,27 +42,48 @@ size_t get_ctls(uintptr_t ctls[]) {
   return i;
 }
 
+uintptr_t get_pawn(uintptr_t ctl) {
+  int handle = read_word(ctl + 0x7b4).int32;
+  uintptr_t pawn_ptr = read_word(read_word(PAWN_ARRAY_ADDR).ptr64 + (handle >> 9 & 0x3f) * 8).ptr64 + (handle & 0x1ff) * 0x78;
+  return read_word(pawn_ptr).ptr64;
+}
+
 static void read_player(uintptr_t ctl, struct player_descriptor *player) {
   uintptr_t local_ctl = read_word(LOCAL_PLAYER_CTL_PTR_ADDR).ptr64;
+  uintptr_t pawn = get_pawn(ctl);
   player->is_local = ctl == local_ctl;
   player->is_alive = read_word(ctl + 0x99c).int32;
   player->team = read_word(ctl + 0x55b).int32 == 2 ? 'T' : 'C';
+  uintptr_t offsets_xyz[] = {0x38, 0x70};
+  uintptr_t ptr_x = hop(MEM_FD, pawn, offsets_xyz, SIZEARR(offsets_xyz));
+  player->x = read_word(ptr_x).float32;
+  player->y = read_word(ptr_x + 4).float32;
+  uintptr_t offsets_yaw[] = {0x10, 0x0, 0x4b0, 0x720,};
+  uintptr_t ptr_y = hop(MEM_FD, pawn, offsets_yaw, SIZEARR(offsets_yaw));
+  player->yaw = read_word(ptr_y).float32;
+}
+
+size_t read_players(struct player_descriptor *players) {
+  uintptr_t ctls[64];
+  size_t ctl_count = get_ctls(ctls);
+  for (int i = 0; i < ctl_count; i++) {
+    read_player(ctls[i], &players[i]);
+  }
+  return ctl_count;
 }
 
 static void print_player(struct player_descriptor *player) {
-  printf("(%d, %c, %d)\n", player->is_local, player->team, player->is_alive);
+  printf("%f,%f,%f:%d,%c,%d\n", player->x, player->y, player->yaw, player->is_local, player->team, player->is_alive);
 }
 
 static void main_loop(void) {
-  uintptr_t ctls[64];
-  struct player_descriptor player;
+  struct player_descriptor players[64];
   for (;;) {
-    size_t ctl_count = get_ctls(ctls);
-    for (int i = 0; i < ctl_count; i++) {
-      read_player(ctls[i], &player);
-      print_player(&player);
+    size_t count = read_players(players);
+    for (int i = 0; i < count; i++) {
+      print_player(&players[i]);
     }
-    msleep(1000);
+    msleep(40);
   }
 }
 
--- xax/src/csrh/ctl_scan.c
+++ xax/src/csrh/ctl_scan.c
@@ -8,45 +8,65 @@
 #include "impl.h"
 #include "math.h"
 
-#define MAX_DEPTH 8
-#define BLOCK_SIZE 1024
-#define PRECISION 0.01
+#define MAX_DEPTH 4
+#define BLOCK_SIZE 2048
+#define PRECISION 0.001
 #define IS_PTR(x) (0x500000000000 < (x) && (x) < 0x800000000000)
 
 size_t get_ctls(uintptr_t ctls[]);
 
-static int MEM_FD;
-static uintptr_t LIBCLIENT_BASE;
+uintptr_t get_pawn(uintptr_t ctl);
+
+extern int MEM_FD;
+extern uintptr_t LIBCLIENT_BASE;
+
 static hashtable *VISITED;
 static uintptr_t CURRENT[MAX_DEPTH];
-static uintptr_t CTLS[64];
+static uintptr_t STARTS[64];
 static float TARGET;
 static uintptr_t START;
 
 static void init_set(void) {
   if (VISITED) {
-    free(VISITED);
+    hash_free(VISITED);
   }
   VISITED = hash_new(BLOCK_SIZE * BLOCK_SIZE, hash_int, hash_cmp_int);
 }
 
+static void init_starts(void) {
+  uintptr_t ctls[64];
+  size_t size = get_ctls(ctls);
+  for (int i = 0; i < size; i++) {
+    STARTS[i] = get_pawn(ctls[i]);
+  }
+}
+
 static void print_result(int d) {
-  hashtable *uniques = hash_new(100, hash_int, hash_cmp_int);
-  for (int i = 0; CTLS[i]; i++) {
-    uintptr_t ptr = hop(MEM_FD, CTLS[i], CURRENT, d);
-    float x = read_mem_word32(MEM_FD, ptr).float32;
-    if (fabs(x) > 1 && IN_RANGE(-5000, x, 5000)) {
-      hash_set(uniques, KV(.uint64 = (int)x), KV(.uint64 = 1));
-    }
+  uintptr_t ptr = hop(MEM_FD, START, CURRENT, d);
+  float x = read_mem_word32(MEM_FD, ptr).float32;
+  printf("%f\n", x);
+  for (int i = 0; i < d; i++) {
+    printf("0x%lx, ", CURRENT[i]);
   }
-  if (uniques->len >= 1) {
-    printf("[%zu] ", uniques->len);
-    for (int i = 0; i < d; i++) {
-      printf("0x%lx, ", CURRENT[i]);
-    }
-    puts("");
-  }
-  hash_free(uniques);
+  puts("");
+//  hashtable *uniques = hash_new(100, hash_int, hash_cmp_int);
+//  for (int i = 0; STARTS[i]; i++) {
+//    uintptr_t ptr = hop(MEM_FD, STARTS[i], CURRENT, d);
+//    float x = read_mem_word32(MEM_FD, ptr).float32;
+//    if (fabs(x) > 0 && IN_RANGE(-5000, x, 5000)) {
+//      printf("%f ", x);
+//      hash_set(uniques, KV(.uint64 = (int)x), KV(.uint64 = 1));
+//    }
+//  }
+//  puts("");
+//  if (uniques->len >= 1) {
+//    printf("[%zu] ", uniques->len);
+//    for (int i = 0; i < d; i++) {
+//      printf("0x%lx, ", CURRENT[i]);
+//    }
+//    puts("");
+//  }
+//  hash_free(uniques);
 }
 
 static void scan(int d, uintptr_t ptr) {
@@ -73,12 +93,14 @@ void ctl_scan() {
   disable_stderr();
   OPEN_MEM("cs2$");
   READ_DS(1536);
-  LIBCLIENT_BASE = get_base_addr(pid, "libclient");
-  get_ctls(CTLS);
   MEM_FD = fd;
+  LIBCLIENT_BASE = get_base_addr(pid, "libclient");
+  printf("0x%lx\n", LIBCLIENT_BASE);
   START = parse_addr(args_get("arg0"));
   TARGET = parse_value(args_get("arg1"), FLOAT32_TYPE).float32;
+  printf("%f\n", TARGET);
   init_set();
+  init_starts();
   scan(0, START);
 }
 
