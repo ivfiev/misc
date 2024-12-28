3ec621ee0065e9754868bba2ea866410a00b6558
ivfiev <filjko@ya.ru>
2024-12-28 21:28:33 +0000

    wip

--- areyou/app/Main.hs
+++ areyou/app/Main.hs
@@ -2,10 +2,21 @@ import Data.Aeson
 import Web.Scotty (jsonData, scotty, liftIO)
 import Web.Scotty.Trans (json, post, middleware, status)
 import GHC.Generics (Generic)
-import Data.IORef (newIORef, readIORef, atomicModifyIORef')
+import Data.IORef (newIORef, readIORef, atomicModifyIORef', IORef)
 import Data.Map qualified as Map
 import Network.Wai (Middleware)
 import Network.HTTP.Types (status404, status422)
+import Web.Scotty.Internal.Types (ActionT)
+import Data.List (maximumBy, minimumBy)
+import Data.Function (on)
+import Crypto.Hash (hashWith, SHA256 (SHA256))
+import Data.ByteString.Char8 qualified as BS
+import Crypto.Cipher.Types (Cipher(cipherInit), BlockCipher (ctrCombine), makeIV)
+import Crypto.Cipher.AES (AES256)
+import Crypto.Error (onCryptoFailure)
+import Data.Maybe (fromMaybe)
+
+type DB = Map.Map String BS.ByteString
 
 data ApiData = ApiData
   { keywords  :: Maybe [String]
@@ -30,43 +41,66 @@ logger app request respond = do
   putStrLn "wip req time"
   app request respond
 
+handleQuery :: IORef DB -> ActionT IO ()
+handleQuery dbRef = do
+  req <- jsonData
+  db <- liftIO $ readIORef dbRef
+  case req of
+    ApiData { keywords = Just kws } -> do
+      let key = hashKeywords kws
+      liftIO $ putStrLn $ hashKeywords kws
+      case Map.lookup key db of
+        Nothing -> do
+          status status404
+          json $ mkError "No message"
+        Just eMsg -> do
+          json $ mkResp kws $ decryptMessage kws eMsg
+    _ -> do
+      status status422
+      json $ mkError "Bad query"
+
+handleCreate :: IORef DB -> ActionT IO ()
+handleCreate dbRef = do
+  payload <- jsonData
+  case payload of
+    ApiData { keywords = Just kws@(_:_), message = Just msg@(_:_) } -> do
+      let key = hashKeywords kws
+      let encrypted = encryptMessage kws msg
+      liftIO $ atomicModifyIORef' dbRef ((,()) . Map.insert key encrypted)
+      json $ mkResp kws msg
+    _ -> do
+      status status422
+      json $ mkError "Bad post"
+
 main :: IO ()
 main = do
   dbRef <- newIORef Map.empty
   scotty 8989 $ do
     middleware logger
-    post "/query" $ do
-      req <- jsonData
-      db <- liftIO $ readIORef dbRef
-      case req of
-        ApiData { keywords = Just kws } -> do
-          let key = hashKeywords kws
-          case Map.lookup key db of
-            Nothing -> do
-              status status404
-              json $ mkError "No message"
-            Just eMsg -> do
-              json $ mkResp kws $ decryptMessage kws eMsg
-        _ -> do
-          status status422
-          json $ mkError "Bad query"
-    post "/create" $ do
-      payload <- jsonData
-      case payload of
-        ApiData { keywords = Just kws@(_:_), message = Just msg@(_:_) } -> do
-          let hash = hashKeywords kws
-          let eMsg = encryptMessage kws msg
-          liftIO $ atomicModifyIORef' dbRef ((,()) . Map.insert hash eMsg)
-          json $ mkResp kws msg
-        _ -> do
-          status status422
-          json $ mkError "Bad post"
+    post "/query" $ handleQuery dbRef
+    post "/create" $ handleCreate dbRef
+
+hash :: String -> String
+hash = show . hashWith SHA256 . BS.pack
 
 hashKeywords :: [String] -> String
-hashKeywords = undefined
+hashKeywords = hash . concat
 
-encryptMessage :: [String] -> String -> String
-encryptMessage = undefined
+mkAES256 :: String -> String -> BS.ByteString -> BS.ByteString
+mkAES256 keyStr ivStr = ctrCombine cipher iv where
+  cipher :: AES256
+  cipher = onCryptoFailure (Prelude.error . show) id $ cipherInit $ mkKey 32 keyStr
+  iv = fromMaybe (Prelude.error "Failed to create IV") $ makeIV $ mkKey 16 ivStr 
+  mkKey len = BS.pack . take len . hash
 
-decryptMessage :: [String] -> String -> String
-decryptMessage = undefined
+mkMessageCipher :: [String] -> BS.ByteString -> BS.ByteString
+mkMessageCipher kws = cipher where
+  shortestKw = minimumBy (compare `on` length) kws
+  longestKw = maximumBy (compare `on` length) kws
+  cipher = mkAES256 longestKw shortestKw
+
+encryptMessage :: [String] -> String -> BS.ByteString
+encryptMessage kws = mkMessageCipher kws . BS.pack
+
+decryptMessage :: [String] -> BS.ByteString -> String
+decryptMessage kws = BS.unpack . mkMessageCipher kws
--- areyou/run.sh
+++
@@ -1,2 +0,0 @@
-cabal build --builddir=/dev/shm/areyou-cabal-build #--verbose
-/dev/shm/areyou-cabal-build/build/x86_64-linux/ghc-9.4.8/areyou-0.1.0.0/x/areyou/build/areyou/areyou
