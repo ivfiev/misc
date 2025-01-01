f7b54f249d30e90e88025d69daafaae253530450
ivfiev <filjko@ya.ru>
2025-01-01 02:35:19 +0000

    wip

--- rugfold/src/Main.hs
+++ rugfold/src/Main.hs
@@ -1,9 +1,10 @@
 module Main where
 import Blockchain (mkChain, addBlock, Blockchain)
 import Data.List (foldl')
+import Server
 
 bc :: Blockchain Int
 bc = foldl' addBlock (mkChain 2) [1..10]
 
 main :: IO ()
-main = print bc
+main = start 8989
--- rugfold/src/Server.hs
+++ rugfold/src/Server.hs
@@ -1,1 +1,29 @@
-module Server where
+module Server(start) where
+
+import Network.Socket
+import Control.Monad (forever, unless)
+import Control.Concurrent (forkFinally)
+import Network.Socket.ByteString (recv)
+import Data.Functor (void)
+import Data.ByteString qualified as BS
+
+start :: Int -> IO ()
+start port = do
+  ai <- getAddrInfo (Just $ defaultHints { addrFlags = [AI_PASSIVE] }) Nothing (Just $ show port)
+  let addr = head ai
+  sock <- socket (addrFamily addr) Stream defaultProtocol
+  setSocketOption sock ReuseAddr 1
+  bind sock (addrAddress addr)
+  listen sock 128
+  forever $ do
+    (peerSock, peerAddr) <- accept sock
+    void $ forkFinally (handle peerSock peerAddr) (const $ close peerSock)
+
+handle :: Socket -> SockAddr -> IO ()
+handle peerSock peerAddr = do
+  bytes <- recv peerSock 4096
+  let len = BS.length bytes
+  print $ show bytes
+  print len
+  unless (len == 0) $ handle peerSock peerAddr
+  
