4bc778601d07672e80dab51b708318936d07d283
ivfiev <filjko@ya.ru>
2025-01-03 22:12:13 +0000

    fixes

--- rugfold/lib/Client.hs
+++ rugfold/lib/Client.hs
@@ -7,24 +7,25 @@ import Network.Socket.ByteString.Lazy
 import Data.ByteString.Lazy qualified as BSL
 import Data.Maybe
 import Utils
+import Control.Exception
 
 data Message a = Output | Add { block :: a }
   deriving (Show, Generic, FromJSON, ToJSON)
 
 sendMsg :: (ToJSON a) => Socket -> Message a -> IO ()
-sendMsg sock msg = send' 0 bytes where
+sendMsg sock msg = catch (sendAll sock bytes) (onSockExn sock) where
   bytes = encode msg <> "\n"
-  send' sent msg
-    | BSL.length msg == sent = return ()
-    | otherwise = do
-      sent' <- send sock msg
-      send' (sent + sent') msg
 
-recvMsg :: (FromJSON a) => Socket -> IO [Message a]
-recvMsg sock = recv' BSL.empty where
+recvMsg :: (FromJSON a) => Socket -> IO (Maybe [Message a])
+recvMsg sock = catch (recv' "") (const (return Nothing) . onSockExn sock) where
   recv' bs = do
-    bytes <- recv sock 4096
-    let bs' = bs <> bytes
-    if "\n" `BSL.isSuffixOf` bs'
-      then return $ mapMaybe decode $ BSL.split (word8 '\n') bs'
-      else recv' bs'
+    r <- recv sock 4096
+    let bs' = bs <> r
+    case r of
+      "" -> return Nothing
+      _ | "\n" `BSL.isSuffixOf` bs' -> 
+        return $ Just $ mapMaybe decode $ BSL.split (word8 '\n') bs'
+      _ -> recv' bs'
+
+onSockExn :: Socket -> SomeException -> IO ()
+onSockExn sock = print
--- rugfold/src/Server.hs
+++ rugfold/src/Server.hs
@@ -23,9 +23,14 @@ server bc port = do
 
 handleConn :: (Show a, FromJSON a, ToJSON a) => MVar (Blockchain a) -> Socket -> SockAddr -> IO ()
 handleConn bcVar peerSock peerAddr = do
-  msgs <- recvMsg peerSock
-  mapM_ (handleMsg bcVar) msgs
-  handleConn bcVar peerSock peerAddr
+  mbMsgs <- recvMsg peerSock
+  case mbMsgs of
+    Just msgs -> do
+      mapM_ (handleMsg bcVar) msgs
+      handleConn bcVar peerSock peerAddr
+    Nothing -> do
+      close peerSock
+      return ()
 
 handleMsg :: (Show a, ToJSON a, FromJSON a) => MVar (Blockchain a) -> Message a -> IO ()
 handleMsg bcVar Output = output bcVar
