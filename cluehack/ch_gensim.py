e4bc741cf369df956578fdff0192a6fe8c354abc
ivfiev <filjko@ya.ru>
2025-02-09 21:15:30 +0000

    wip

--- cluehack/ch_gensim.py
+++ cluehack/ch_gensim.py
@@ -1,16 +1,37 @@
+import itertools
 import gensim.downloader as api
 
 model = api.load('word2vec-google-news-300')
 
 def assocs(word):
-  one = model.most_similar(word, topn=20)
-  two = {t[0] for w in one for t in model.most_similar(w[0], topn=10)}
-  return two
+  return [w for (w, _) in model.most_similar(word, topn=20) if '_' not in w][:10]
 
-def clues(word1, word2, n):
-  a1 = assocs(word1)
-  a2 = assocs(word2)
-  cs = [(model.similarity(a10, a20), a10, a20) for a10 in a1 for a20 in a2]
+def clues(words, n):
+  ass = [assocs(w) for w in words]
+  tups = itertools.product(*ass)
+  cs = [(eval_group(t), t) for t in tups]
   return sorted(cs, key=lambda t: -t[0])[:n]
 
-print(clues('wolf', 'dog', 20))
+def eval_group(words):
+  sum = 0.0
+  for i in range(len(words)):
+    for j in range(i + 1, len(words)):
+      sum += model.similarity(words[i], words[j])
+  return round(sum * 2 / len(words) / (len(words) - 1), 3)
+
+def get_groups(words, max):
+  gs = []
+  for n in range(2, max + 1):
+    gs.extend(itertools.combinations(words, n))
+  return gs
+
+def cheat(words):
+  groups = get_groups(words, 4)
+  evald = map(lambda g: (eval_group(g), g), groups)
+  best = sorted(evald, key=lambda t: -t[0])
+  print(words)
+  for g in best[:20]:
+    print(g)
+    for w in g[1]:
+      print(assocs(w))
+    print()
