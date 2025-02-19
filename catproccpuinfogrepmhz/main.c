9f292e8ebea0964e1d9c1c862bc799d2410f5703
ivfiev <filjko@ya.ru>
2025-02-19 19:04:17 +0000

    add load(50) column

--- catproccpuinfogrepmhz/main.c
+++ catproccpuinfogrepmhz/main.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <signal.h>
+#include <math.h>
 #define SAMPLES_PER_SEC 10
 #define SAMPLES_RING 50
 
@@ -117,15 +118,43 @@ void handle_sigint(void) {
   }
 }
 
+void calc_ring_stats(int ring[][SAMPLES_RING], int maxes[], float avgs[], int cpus) {
+  for (int i = 0; i < cpus; i++) {
+    int max = 0;
+    float avg = 0;
+    for (int j = 0; j < SAMPLES_RING; j++) {
+      max = ring[i][j] > max ? ring[i][j] : max;
+      avg += ring[i][j];
+    }
+    avg /= SAMPLES_RING;
+    avgs[i] = avg;
+    maxes[i] = max;
+  }
+}
+
+void calc_loads(int min_freq, float avgs[], int loads[], int cpus) {
+  const int stages = 5;
+  float max = -10e9;
+  for (int i = 0; i < cpus; i++) {
+    max = avgs[i] > max ? avgs[i] : max;
+  }
+  float range = ceil(1 + (max - min_freq) / stages);
+  for (int i = 0; i < cpus; i++) {
+    loads[i] = 1 + (int)((avgs[i] - min_freq) / range);
+  }
+}
+
 int main(void) {
   handle_sigint();
   int cpus = get_cpus(), samples = 0;
-  int indexes[cpus], clocks[cpus], maxes[cpus], ring[cpus][SAMPLES_RING];
-  float avgs[cpus];
+  int indexes[cpus], clocks[cpus], maxes[cpus], maxes_ring[cpus], ring[cpus][SAMPLES_RING], loads[cpus];
+  float avgs[cpus], avgs_ring[cpus];
+  int min_avg_freq = 0xFFFF;
 
   memset(avgs, 0, cpus * sizeof(float));
   memset(maxes, 0, cpus * sizeof(int));
   memset(ring, 0, cpus * SAMPLES_RING * sizeof(int));
+  memset(loads, 0, cpus * sizeof(int));
   index_cpuinfo(indexes, cpus);
 
   for (;;) {
@@ -141,21 +170,28 @@ int main(void) {
       samples++;
       usleep(1000000 / SAMPLES_PER_SEC);
     }
-    
+
     printf("\e[1;1H\e[2J");
-    printf("core#\tnow\tmax(%d)\tavg(%d)\tmax(*)\tavg(*)\n", SAMPLES_RING, SAMPLES_RING);
+    printf("core#\tnow\tmax(%d)\tavg(%d)\tmax(*)\tavg(*)\tload(50)\n", SAMPLES_RING, SAMPLES_RING);
+
+    calc_ring_stats(ring, maxes_ring, avgs_ring, cpus);
+    
+    if (samples >= SAMPLES_RING) {
+      for (int i = 0; i < cpus; i++) {
+        min_avg_freq = avgs_ring[i] < min_avg_freq ? avgs_ring[i] : min_avg_freq;
+      }
+
+      calc_loads(min_avg_freq, avgs_ring, loads, cpus);
+    }
 
     for (int i = 0; i < cpus; i++) {
+      printf("%d\t%d\t%d\t%d\t%d\t%d\t", i, clocks[i], maxes_ring[i], (int)avgs_ring[i], maxes[i], (int)avgs[i]);
 
-      int max = 0;
-      float avg = 0;
-      for (int j = 0; j < SAMPLES_RING; j++) {
-        max = ring[i][j] > max ? ring[i][j] : max;
-        avg += ring[i][j];
+      for (int j = 0; j < loads[i]; j++) {
+        putchar('*');
       }
-      avg /= SAMPLES_RING;
 
-      printf("%d\t%d\t%d\t%d\t%d\t%d\n", i, clocks[i], max, (int)avg, maxes[i], (int)avgs[i]);
+      putchar('\n');
     }
   }
 
