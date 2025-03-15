e706e54bb731bebb5fda3722763acc8e6230b978
ivfiev <filjko@ya.ru>
2025-03-15 14:12:01 +0000

    wip

--- go-warnings/src/main.c
+++ go-warnings/src/main.c
@@ -10,26 +10,30 @@
 void handle_syscall(pid_t pid) {
   struct user_regs_struct regs;
   ptrace(PTRACE_GETREGS, pid, 0, &regs);
-  printf("[+] Syscall %lld intercepted (RAX=%lld)\n", regs.orig_rax, regs.rax);
-
-  if (regs.orig_rax == 1) {  // execve syscall on x86_64
-      printf("intercepted write(2)");
-      // regs.rdi = (unsigned long)"/bin/echo";  // Change execve to /bin/echo
-      // ptrace(PTRACE_SETREGS, pid, 0, &regs);
+  if (regs.orig_rax == 1) {
+    printf("write(2) pid: [%d]\n", pid);
+  }
+  if (regs.orig_rax == 56) {
+    printf("clone(2) pid: [%d]\n", pid);
+  }
+  if (regs.orig_rax == 59) {
+    printf("execve(2) pid: [%d]\n", pid);
   }
 }
 
-void trace_process(pid_t child) {
+void trace_process(pid_t process) {
   int status;
   while (1) {
-      waitpid(child, &status, 0);
-      if (WIFEXITED(status)) break;
-
-      if (WSTOPSIG(status) == SIGTRAP) {
-          handle_syscall(child);
+    pid_t child = waitpid(-1, &status, 0);
+    if (WIFEXITED(status)) {
+      if (child == process) {
+        break;
+      } else {
+        continue;
       }
-
-      ptrace(PTRACE_SYSCALL, child, 0, 0);
+    }
+    handle_syscall(child);
+    ptrace(PTRACE_SYSCALL, child, 0, 0);
   }
 }
 
@@ -38,16 +42,15 @@ int main(int argc, char **argv) {
     fprintf(stderr, "Usage: %s /path/to/program\n", argv[0]);
     return 1;
   }
-
   pid_t child = fork();
   if (child == 0) {
-      ptrace(PTRACE_TRACEME, 0, 0, 0);
-      return execvp(argv[1], &argv[1]);
+    ptrace(PTRACE_TRACEME, 0, 0, 0);
+    return execvp(argv[1], &argv[1]);
   } else {
-      waitpid(child, NULL, 0);
-      ptrace(PTRACE_SETOPTIONS, child, 0,
-            PTRACE_O_TRACEFORK | PTRACE_O_TRACEEXEC | PTRACE_O_TRACECLONE);
-      trace_process(child);
+    waitpid(child, NULL, 0);
+    ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_TRACEFORK | PTRACE_O_TRACEVFORK | PTRACE_O_TRACECLONE);
+    ptrace(PTRACE_SYSCALL, child, 0, 0);  
+    trace_process(child);
   }
 
 return 0;
