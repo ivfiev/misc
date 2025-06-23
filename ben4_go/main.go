e81a1d859543d618fdb676394da0da092ceed974
ivfiev <filjko@ya.ru>
2025-06-23 22:08:16 +0100

    wip

--- ben4_go/main.go
+++ ben4_go/main.go
@@ -11,11 +11,11 @@ import (
 var FINISH bool = false
 
 func sleep(secs int) {
-  time.Sleep(time.Duration(secs) * time.Second)
-  FINISH = true
+	time.Sleep(time.Duration(secs) * time.Second)
+	FINISH = true
 }
 
-func work_int(results chan int32) {
+func work_int() int32 {
 	var primes [1229]int32
 	var count, k int32
 	for count = 0; !FINISH; count++ {
@@ -26,22 +26,22 @@ func work_int(results chan int32) {
 		k = 1
 		for n := int32(3); n < 10000; n += 2 {
 			for i := range k {
-				if n % primes[i] == 0 {
+				if n%primes[i] == 0 {
 					break
 				}
-				if n < primes[i] * primes[i] {
+				if n < primes[i]*primes[i] {
 					primes[k] = n
 					k++
-          break
+					break
 				}
 			}
 		}
 	}
 	fmt.Printf("%d ", k)
-  results <- count
+	return count
 }
 
-func work_float(results chan int32) {
+func work_float() int32 {
 	var primes [1229]int32
 	var count, k int32
 	for count = 0; !FINISH; count++ {
@@ -52,48 +52,49 @@ func work_float(results chan int32) {
 		k = 1
 		for n := int32(3); n < 10000; n += 2 {
 			for i := range k {
-        if int32(float32(n) / float32(primes[i])) * primes[i] == n {
-          break
-        }
-        if primes[i] * primes[i] > n {
-          primes[k] = n
-          k++;
-          break;
-        }
+				if int32(float32(n)/float32(primes[i]))*primes[i] == n {
+					break
+				}
+				if primes[i]*primes[i] > n {
+					primes[k] = n
+					k++
+					break
+				}
 			}
 		}
 	}
 	fmt.Printf("%d ", k)
-  results <- count
+	return count
 }
 
 func main() {
-  if len(os.Args) < 3 {
-    log.Fatalf("usage 'ben4_go i/f #threads'")
-  }
-  if os.Args[1] != "i" && os.Args[1] != "f" {
-    log.Fatalf("incorrect numeric type %s\n", os.Args[1])
-  }
-  threads, err := strconv.Atoi(os.Args[2])
-  if err != nil {
-    log.Fatalf("#threads [%s] must be a number\n", os.Args[0])
-  }
-  if threads < 0 || threads > 100 {
-    log.Fatalf("bad #threads [%d]\n", threads)
-  }
-  work := work_int
-  if os.Args[1] == "f" {
-    work = work_float
-  }
-  var sum int32 = 0
-  results := make(chan int32)
+	if len(os.Args) < 3 {
+		log.Fatalf("usage 'ben4_go i/f #threads'")
+	}
+	if os.Args[1] != "i" && os.Args[1] != "f" {
+		log.Fatalf("incorrect numeric type %s\n", os.Args[1])
+	}
+	threads, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Fatalf("#threads [%s] must be a number\n", os.Args[0])
+	}
+	if threads < 0 || threads > 100 {
+		log.Fatalf("bad #threads [%d]\n", threads)
+	}
+	work := work_int
+	if os.Args[1] == "f" {
+		work = work_float
+	}
+	var sum int32 = 0
+	results := make(chan int32)
 	go sleep(10)
-  for range threads {
-    go work(results)
-  }
-  for range threads {
-    res := <-results
-    sum += res
-  }
-  fmt.Printf("\n%d\n", sum)
+	for range threads {
+		go func() {
+			results <- work()
+		}()
+	}
+	for range threads {
+		sum += <-results
+	}
+	fmt.Printf("\n%d\n", sum)
 }
