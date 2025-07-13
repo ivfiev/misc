ebd34a6d3f03dbcee4bf0b41800608cbd733d113
ivfiev <filjko@ya.ru>
2025-07-13 12:25:07 +0100

    poc

--- msr/main.c
+++ msr/main.c
@@ -1,33 +1,74 @@
 #include <stdio.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <stdint.h>
 
+#define OFFSET_POWER_UNIT 0xc0010299
 #define OFFSET_CORE_ENERGY 0xc001029a
 #define OFFSET_PACKAGE_ENERGY 0xc001029b
 
-#define ENERGY_UNIT (15.3 / 1000000)
+#define CPUS 32
 
-int main(void) {
-	int fd = open("/dev/cpu/0/msr", O_RDONLY);
+static float ENERGY_UNIT;
+
+static int open_msr(int cpu) {
+	char str[32];
+	snprintf(str, sizeof(str), "/dev/cpu/%d/msr", cpu);
+	int fd = open(str, O_RDONLY);
 	if (fd < 0) {
 		perror("could not open msr");
-		return -1;
+		exit(-2);
 	}
+	return fd;
+}
+
+static uint64_t read_msr(int fd, __off64_t offset) {
 	uint8_t buf[8];
-	uint64_t previous = 0;
+	int read = pread(fd, buf, sizeof(buf), offset);
+	if (read != 8) {
+		perror("could not read msr");
+		exit(-1);
+	}
+	return *(uint64_t *)buf;
+}
+
+int main(void) {
+	printf("%.1f\n", 1.81);
+	int fds[CPUS];
+	uint64_t pkg0 = 0, pkg1 = 0;
+	uint64_t cpu0[CPUS] = {0}, cpu1[CPUS] = {0};
+	for (int i = 0; i < CPUS; i++) {
+		fds[i] = open_msr(i);
+	}
+	uint64_t unit = read_msr(fds[0], OFFSET_POWER_UNIT);
+	ENERGY_UNIT = 1.0 / (1 << ((unit >> 8) & 0x1F));
+	
 	for (;;) {
-		int read = pread(fd, buf, 8, OFFSET_PACKAGE_ENERGY);
-		if (read != 8) {
-			perror("could not read msr");
-			return -2;
+
+		// package
+		pkg0 = read_msr(fds[0], OFFSET_PACKAGE_ENERGY);
+		if (pkg1) {
+			uint64_t delta = pkg0 - pkg1;
+			float watts = ENERGY_UNIT * delta;
+			printf("package power: [%f]\n", watts);
 		}
-		uint64_t units = *(uint64_t *)buf;
-		if (previous) {
-			float watts = (units - previous) * ENERGY_UNIT;
-			printf("raw units: [0x%lx], watts: [%f]\n", units, watts);
+		pkg1 = pkg0;
+
+		// per-core
+		for (int i = 0; i < CPUS; i++) {
+			if (i == CPUS / 2) puts("");
+			cpu0[i] = read_msr(fds[i], OFFSET_CORE_ENERGY);
+			if (cpu1[i]) {
+				uint64_t delta = cpu0[i] - cpu1[i];
+				float watts = ENERGY_UNIT * delta;
+				printf("[%f], ", watts);
+			}
+			cpu1[i] = cpu0[i];
 		}
-		previous = units;
+
+		puts("");
 		sleep(1);
 	}
 }
