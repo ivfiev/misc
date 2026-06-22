eb10dd786058a24ec21cbfd8f17eac5653efffbd
ivfiev <11612131+ivfiev@users.noreply.github.com>
2026-06-22 20:48:25 +0100

    wip

--- T-lang/main.py
+++ T-lang/main.py
@@ -4,10 +4,3 @@ from programs import *
 
 if __name__ == "__main__":
     run_tests()
-    # next
-    # strawberry
-    # feature allocator. BROADCAST = alloc()
-    # reverse a string. abcd|????
-    # contains substring. halves equal. letter counts equal
-    # all/exists, inc/dec/dist k, more arithmetic
-    # maybe one day textual language & descriptive what-happened output
